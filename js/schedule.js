2a516f68cdd3ddacbe6ae42422df82c313ce40c8
ctLim1214 <264381242+lim-ctlim1214@users.noreply.github.com>
2026-04-24 13:29:16 +0800

    FEAT: Added JS and Partial Media Queries to Schedule

---
+++ js/schedule.js
@@ -0,0 +1,44 @@
+const schedfinder = document.getElementById("schedfinder")
+const availability_indicator = document.getElementById("availability-indicator")
+
+schedfinder.addEventListener("click", (e) => {
+    availability_indicator.classList.remove("hidden")
+    if (getSched()){
+        availability_indicator.innerHTML = "<strong>I am!</strong> Feel free to contact me!"
+    } else {
+        availability_indicator.innerHTML = "<strong style='color:red;'>I am not!</strong> Feel free to contact me still, I will read your message regardless."
+    }
+})
+
+let today = new Date();
+function mobile_support(){
+    if (window.innerWidth > 1000) return
+    let selector = "." + today.toLocaleDateString('en-US', {
+        weekday: 'long'
+    }).toLowerCase()
+
+    let table_elements = document.querySelectorAll(selector)
+    table_elements.forEach((element) => {
+        element.classList.remove(selector)
+        element.style.display = 'block'
+    })
+
+    let title_heading = document.getElementById("title-heading")
+    title_heading.innerHTML = "Cedric's Schedule for Today!";
+}
+
+mobile_support()
+
+
+function getSched(){
+
+    let date = today.getDay()
+    let time = today.getHours()
+
+    if (date === 0 || date === 4) return true;
+    else {
+        if (date === 6 && time >= 11) return true;
+        if (time >= 6 && time < 18) return false; //7-7 unavailable
+        return true;
+    }
+}
